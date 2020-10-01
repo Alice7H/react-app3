1644e972f2ce462f7ce5bf1c740d52ec0badfbaf
Alice Hata <alicehata01@hotmail.com>
2020-10-01 15:34:36 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+ const [count, setCount] = useState(0);
+
+ // similar a componentDidMount e componentDidUpdate:
+useEffect(() => {
+ // atualiza o título do documento 
+ document.title = `Você clicou ${count} times`;
+});
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Você clicou {count} vezes</p>
+        <button onClick={()=>setCount(count + 1)}>Clique</button>
       </header>
     </div>
   );
