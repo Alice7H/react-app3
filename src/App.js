e16250ebcb4866bb2a4a67cb861edc00bc381be7
Alice Hata <alicehata01@hotmail.com>
2022-05-11 11:56:51 -0300

    Update README.md
    Updated README.md
    Removed some styles and comments.
    Removed documents about tests, service worker and logo svg .
    Changed lang from index.html
    Created custom buttons
    Added reset button

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-
  const [count, setCount] = useState(0);
 
- // similar a componentDidMount e componentDidUpdate:
 useEffect(() => {
- // atualiza o título do documento 
- document.title = `Você clicou ${count} times`;
+  document.title = `Você clicou ${count} times`;
 });
 
   return (
     <div className="App">
       <header className="App-header">
         <p>Você clicou {count} vezes</p>
-        <button onClick={()=>setCount(count + 1)}>Clique</button>
+        <button type="button" onClick={()=>setCount(count + 1)}>Adiciona</button>
+        <button type="button" onClick={()=>setCount(0)}>Reestabelecer o contador</button>
       </header>
     </div>
   );
